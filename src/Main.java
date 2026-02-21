5d053df8ad49e6634e4d5f8747be653e2e093d0a
chps666 <thomasedsonv22@gmail.com>
2026-02-20 21:21:53 -0300

    Exercicio manipulacao de variaveis

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String numero = "6";
+        int number = Integer.parseInt(numero);
+        number += 10;
+        System.out.println(number);
+    }
+}
